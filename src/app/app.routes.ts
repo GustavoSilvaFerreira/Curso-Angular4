7e9b687090045709385e827682ba5f08e45c210b
unknown <guto7626@hotmail.com>
2017-10-03 11:31:28 -0300

    Criação do componente individual de restaurante

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from '@angular/router'
 
 import {HomeComponent} from './home/home.component'
+import {RestaurantsComponent} from './restaurants/restaurants.component'
 import {AboutComponent} from './about/about.component'
 
 export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'restaurants', component: RestaurantsComponent},
+  {path: 'about', component: AboutComponent},
 ]

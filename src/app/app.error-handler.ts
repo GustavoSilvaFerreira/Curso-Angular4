b635a977eba04184ea4269609c177b773affd28b
Gustavo da Ferreira <gustavo.da.ferreira@avanade.com>
2019-08-08 10:55:17 -0300

    refatorando de http para httpClient

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,13 +1,15 @@
-import { Response } from '@angular/http'
-import {Observable} from 'rxjs/Observable'
+import { HttpErrorResponse } from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
 
-export class ErrorHandler{
-  static handleError(error: Response | any){
+export class ErrorHandler {
+  static handleError(error: HttpErrorResponse | any) {
     let errorMessage: string
-    if(error instanceof Response){
-      errorMessage = `Erro ${error.status} ao acessar a url ${error.url} - ${error.statusText}`
-    }else{
-      errorMessage = error.toString()
+    if (error instanceof HttpErrorResponse) {
+      const body = error.error;
+      errorMessage = `${error.url}: ${error.status} - ${error.statusText || ''} ${body}`;
+    } else {
+      errorMessage = error.message ? error.message : error.toString();
     }
     console.log(errorMessage)
     return Observable.throw(errorMessage)

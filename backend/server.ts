714439dc1d24e3a141fe05e3781a2e3fab1769c3
Gustavo da Ferreira <gustavo.da.ferreira@avanade.com>
2019-08-08 12:09:02 -0300

    autenticação no json-server - simulando backend

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,8 @@ import { Express } from 'express';
 import * as fs from 'fs';
 import * as https from 'https';
 
+import { handleAuthentication } from './auth';
+
 const server: Express = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -16,6 +18,8 @@ server.use(middlewares);
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
 
+// middleware para login
+server.post('/login', handleAuthentication);
 
 // Use default router
 server.use(router)
---
+++ backend/users.ts
@@ -0,0 +1,12 @@
+export class User {
+  constructor(public email: string, public name: string, public password: string) {}
+
+  matches(another: User): boolean {
+    return another !== undefined && another.email === this.email && another.password === this.password;
+  }
+}
+
+export const users = {
+  "juliana@gmail.com": new User('juliana@gmail.com', 'Juliana', 'juliana23'),
+  "amanda@gmail.com": new User('amanda@gmail.com', 'Amanda', 'amanda21')
+}

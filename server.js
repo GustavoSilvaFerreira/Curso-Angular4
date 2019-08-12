2b70efb30b6c1785b93c276a07b7465cee5e2ff6
Gustavo da Ferreira <gustavo.da.ferreira@avanade.com>
2019-08-12 17:36:23 -0300

    teste porta

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-// app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
 
-// res.sendFile(path.join(__dirname+'/dist/index.html'));
-// });
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 3001);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);

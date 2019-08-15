cfb3046deaaa6a6ed6f40053edadf819e60befb0
Gustavo da Ferreira <gustavo.da.ferreira@avanade.com>
2019-08-15 12:11:10 -0300

    front

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -8,7 +8,7 @@ var authz_1 = require("./authz");
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
-var port = 3001;
+var port = process.env.PORT || 4000;
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 // To handle POST, PUT and PATCH you need to use a body-parser
